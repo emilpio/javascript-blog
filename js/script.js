11b227758f1fb66310fbc282ec9418ed3e150b85
Emil Pio <emilpio@gmail.com>
2022-08-26 16:22:58 +0200

    fix middle column

--- js/script.js
+++ js/script.js
@@ -3,18 +3,17 @@
 function titleClickHandler(event) {
     console.log("Link was clicked!");
     console.log(event);
+    event.preventDefault();
 
     /* remove class 'active' from all article links  */
 
-    const activeLinks = document.querySelectorAll(".titles a.active");
+    const activeLinks = document.querySelectorAll("titles ");
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove("active");
     }
 
     /* add class 'active' to the clicked link */
-
-    event.preventDefault();
 
     const clickedElement = this;
     console.log("clickedElement:" + clickedElement);
@@ -24,7 +23,7 @@ function titleClickHandler(event) {
 
     /* remove class 'active' from all articles */
 
-    const activeArticles = document.querySelectorAll(".posts a.active");
+    const activeArticles = document.querySelectorAll(".post ");
 
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove("active");
@@ -43,6 +42,7 @@ function titleClickHandler(event) {
     /* add class 'active' to the correct article */
 
     targetArticle.classList.add("active");
+    console.log("corecct class to article ");
 }
 
 const links = document.querySelectorAll(".titles a");
